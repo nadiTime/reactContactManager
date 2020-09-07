1e559201f1b2fb6cca56b55f80fe24e334c97452
nadiTime <nadavcn@gmail.com>
2020-09-07 18:24:11 +0300

    Updates

---
+++ precache-manifest.96281cbfceff73d9d73364217ce0d754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac91367c2c87d6bf31253aa832d44ee",
+    "url": "/reactContactManager/index.html"
+  },
+  {
+    "revision": "706cbf774b04ecf56125",
+    "url": "/reactContactManager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a7edfbe45e9f3c4dc68e",
+    "url": "/reactContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "706cbf774b04ecf56125",
+    "url": "/reactContactManager/static/js/2.59e09ffc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactContactManager/static/js/2.59e09ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7edfbe45e9f3c4dc68e",
+    "url": "/reactContactManager/static/js/main.f25dc5df.chunk.js"
+  },
+  {
+    "revision": "8aeb690b57e881ec05a3",
+    "url": "/reactContactManager/static/js/runtime-main.c567e1fb.js"
+  }
+]);
